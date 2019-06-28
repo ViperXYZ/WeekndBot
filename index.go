f3a523830d79b0dc88087dfb332d4e7793738d56
ViperXYZ <sammy.haque@mail.utoronto.ca>
2019-06-28 05:23:44 -0400

    switchted to nlopes slack library, rewrote handler

--- index.go
+++ index.go
@@ -2,26 +2,51 @@ package WeekndBot
 
 import (
 	"encoding/json"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"github.com/nlopes/slack"
+	"os"
 )
 
-type testStruct struct {
-	Test string
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var t testStruct
-	err := decoder.Decode(&t)
-	if err != nil {
-		log.Fatalf("Could not deserialize JSON request to struct: %v", err)
+	signingSecret, ok := os.LookupEnv("SIGNING_SECRET")
+	if !ok {
+		log.Println("SIGNING_SECRET not declared")
+		return
 	}
-	log.Println(t.Test)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	err = json.NewEncoder(w).Encode(t)
+	verifier, err := slack.NewSecretsVerifier(r.Header, signingSecret)
 	if err != nil {
-		log.Fatalf("Could not serialize and send repsonse struct: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+
+	r.Body = ioutil.NopCloser(io.TeeReader(r.Body, &verifier))
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err = verifier.Ensure(); err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	switch s.Command {
+	case "/weekndsays":
+		params := &slack.Msg{Text: s.Text}
+		b, err := json.Marshal(params)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(b)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 }
